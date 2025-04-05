e05e6996880c49847e0a71ff380bfa38c8f5a181
Nikita Gritsanyuk <doting.laws0d@icloud.com>
2025-04-05 20:52:11 +0800

    Added functions for working with problems

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from uuid import UUID
 import psycopg2
 from psycopg2 import Error
@@ -23,7 +24,7 @@ async def add_user(first_name, last_name, telegram_id) -> UUID:
         if db_connection:
             db_connection.close()
 
-async def get_user(uuid: UUID, tid: int) -> list:
+async def get_user(uuid: UUID = None, tid: int = None) -> list:
     if not uuid and not tid:
         raise ValueError("Either uuid or tid must be provided.")
     try:
@@ -45,7 +46,55 @@ async def get_user(uuid: UUID, tid: int) -> list:
         if db_connection:
             db_connection.close()
             
-# async def get_problems()
+
+async def add_problem(problem_number: str, assigned_to: datetime) -> UUID:
+    try:
+        db_connection = connect_db()
+        cursor = db_connection.cursor()
+        cursor.execute("""
+            INSERT INTO problems (problem_number, assigned_to)
+            VALUES (%s, %s)
+            RETURNING id;
+        """, (problem_number, assigned_to))
+        problem_id = cursor.fetchone()[0]
+        db_connection.commit()
+        return problem_id
+    except Error as e:
+        logging.error(f"[Error adding user]: {e}")
+        raise
+    finally:
+        if db_connection:
+            db_connection.close()
+
+async def get_problems() -> list:
+    try:
+        db_connection = connect_db()
+        cursor = db_connection.cursor()
+        cursor.execute("SELECT * FROM problems;")
+        return cursor.fetchall()
+    except Error as e:
+        logging.error(f"[Error getting problems]: {e}")
+        raise
+    finally:
+        if db_connection:
+            db_connection.close()
+
+async def get_assigned_problems() -> list:
+    try:
+        db_connection = connect_db()
+        cursor = db_connection.cursor()
+        cursor.execute("""
+            SELECT upm.user_id, p.problem_number
+            FROM users_problems_map upm
+            JOIN problems p ON upm.problem_id = p.id;
+        """)
+        return cursor.fetchall()
+    except Error as e:
+        logging.error(f"[Error getting user UUIDs and problem numbers]: {e}")
+        raise
+    finally:
+        if db_connection:
+            db_connection.close()
 
 def connect_db():
     return psycopg2.connect(database=DATABASE_NAME, user=DATABASE_USERNAME, 
